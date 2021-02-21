58f27750126ded3a160a50134ed6616194dc3a56
cristovaobcc <cristovao.bcc@gmail.com>
2021-02-20 22:36:31 -0300

    Video Java - Swing [2/4]: Criando janelas em Swing
    link: https://www.youtube.com/watch?v=Wo8QMwywTXI&list=PLH_lE515NYR0lRYClKMkS7l6lyKjI_a5-&index=2

---
+++ src/app/App.java
@@ -0,0 +1,42 @@
+package app;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		// Lembrando de JFrame e suas propriedades básicas...
+		JFrame f = new JFrame();
+		f.setTitle("Janela");
+		f.setSize(300, 200);
+		f.setLocation(500, 300);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Lembrando do JPanel e outros componentes...
+		JPanel p = new JPanel(); // é um componente invisível que serve para add outros componentes
+		
+		JLabel l = new JLabel("Digite seu nome:");
+		p.add(l);
+		
+		JTextField t = new JTextField(10);
+		p.add(t);
+		
+		JButton b = new JButton("OK");
+		BotaoAction action = new BotaoAction(t);
+		b.addActionListener(action);
+		p.add(b);
+		
+		
+		f.add(p);
+		f.setVisible(true);
+		
+		
+		
+	}
+
+}
